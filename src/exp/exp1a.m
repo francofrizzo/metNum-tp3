28188a8b2efce6c90235890a0273538a9fd9e364
Franco Frizzo <francofrizzo@gmail.com>
2015-11-10 15:08:27 -0300

    Secciones 1 y 2 del informe (falta un poco en la 2)

--- src/exp/exp1a.m
+++ src/exp/exp1a.m
@@ -1,3 +1,8 @@
+% Definicion de colores
+rojo = [197 66 25] ./ 255;
+verde = [81 179 33] ./ 255;
+azul = [18 113 156] ./ 255;
+
 % Procesado de los datos
 [duracion_x_a, tiempos_y_a, e_a, cant_a] = leer_datos('exp1/exp1-a-0.txt');
 [duracion_x_b, tiempos_y_b, e_b, cant_b] = leer_datos('exp1/exp1-a-1.txt');
@@ -6,15 +11,28 @@
 % Creación de los gráficos
 filetype='-dpdf';
 mkdir('graficos');
-figure;
+h = figure(1);
+
+grid on;
 
 hold on;
-h = errorbar(duracion_x_a, tiempos_y_a, e_a, '-r');
-errorbar(duracion_x_b, tiempos_y_b, e_b, '-g');
-errorbar(duracion_x_c, tiempos_y_c, e_c, '-b');
-xlabel('Tiempo de duracion del video en segundos','FontSize',12);
-ylabel('Tiempo de ejecucion en ticks de clock','FontSize',10);
-legend('Vecino mas cercano','Lineal','Spline','Location','northwest');
-set(get(h, 'Parent'), 'YScale', 'log');
-hold off;
+hVecino = errorbar(duracion_x_a, tiempos_y_a, e_a, '-r');
+hLineal = errorbar(duracion_x_b, tiempos_y_b, e_b, '-g');
+hSpline = errorbar(duracion_x_c, tiempos_y_c, e_c, '-b');
+hXLabel = xlabel('Tiempo de duracion del video en segundos','FontSize',12);
+hYLabel = ylabel('Tiempo de ejecucion en ticks de clock','FontSize',10);
+hLegend = legend('Vecino mas cercano','Lineal','Spline','Location','northwest');
+%   set(get(hVecino, 'Parent'), 'YScale', 'log');
+
+set([gca; findall(gca, 'Type','text')], 'FontName', 'Carlito');
+
+set(hVecino, 'Color', rojo);
+set(hLineal, 'Color', verde);
+set(hSpline, 'Color', azul);
+
+yt = get(gca,'YTick');
+set(gca,'YTickLabel', sprintf('%d|',yt))
+
 print('graficos/exp1a', filetype);
+
+hold off;
